adbf8db5e99bd6daac8a99067c29d0557e148149
LarissaRC <50839355+LarissaRC@users.noreply.github.com>
2023-01-20 13:48:34 -0400

    Realizar login

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import sqlite3
-from flask import Flask, session, render_template, request, g, flash
+import querys
+from flask import Flask, session, render_template, request, g, flash, redirect, url_for
 
 app = Flask(__name__)
 app.secret_key = "greenwhaleonthesky"
@@ -9,18 +10,42 @@ app.config["SESSION_COOKIE_NAME"] = "catsleepsonthetree45"
 def index():
     return render_template("index.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    if request.method == "POST":
+        user_email = request.form["user_email"]
+        user_password = request.form["user_password"]
+        
+        user_id = querys.get_user(user_email, user_password)
+        if(user_id):
+            session["user_id"] = user_id
+            session.modified = True
+            return redirect(url_for("home"))
+    else:
+        return render_template("login.html")
 
-@app.route("/signgup")
+@app.route("/signgup", methods=["GET", "POST"])
 def logup():
-    return render_template("logup.html")
+    if request.method == "POST":
+        user_name = request.form["user_name"]
+        user_email = request.form["user_email"]
+        user_password = request.form["user_password"]
+        querys.register_user(user_name, user_email, user_password)
+        user_id = querys.get_user(user_email, user_password)
 
-@app.route("/home", methods=["get"])
+        if(user_id):
+            session["user_id"] = user_id
+            session.modified = True
+            return redirect(url_for("home"))
+    else:
+        return render_template("logup.html")
+
+@app.route("/home", methods=["GET"])
 def home():
-    codigos, titulos, descricoes, anos, meses, dias = get_db_eventos()
+    user_id = session["user_id"]
+    codigos, titulos, descricoes, anos, meses, dias = querys.get_events(user_id)
     eventos = []
+    
     for i in range(len(codigos)):
         eventos.append((codigos[i], titulos[i], descricoes[i], anos[i], meses[i], dias[i]))
 
@@ -28,33 +53,6 @@ def home():
     session.modified = True
 
     return render_template("home.html", eventos = session["eventos"])
-
-def get_db_eventos():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect('agenda.db')
-        cursor = db.cursor()
-        cursor.execute("select * from Evento")
-        all_data = cursor.fetchall()
-        codigos = [str(val[0]) for val in all_data]
-        titulos = [str(val[1]) for val in all_data]
-        descricoes = [str(val[2]) for val in all_data]
-        datas = [str(val[3]) for val in all_data]
-        anos = [val[:4] for val in datas]
-        meses = [val[5:7] for val in datas]
-        dias = [val[8:10] for val in datas]
-
-    return codigos, titulos, descricoes, anos, meses, dias
-
-def get_db_users(email, senha):
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect('agenda.db')
-        cursor = db.cursor()
-        cursor.execute("select * from User where email='" + email + "' and senha='" + senha + "'")
-        user = cursor.fetchall()
-
-    return user
 
 if __name__ == '__main__':
     app.run()
